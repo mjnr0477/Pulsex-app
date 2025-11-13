f7d1f3e94c5d5e847d6a4946c68f152dbf150bba
Mathias Simiyu <mathiassimiyu52@gmail.com>
2025-11-13 17:38:32 +0300

    Add all Pulsex files + workflow

---
+++ metro.config.js
@@ -0,0 +1,10 @@
+const { getDefaultConfig } = require('@expo/metro-config');
+const path = require('path');
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../');
+
+const config = getDefaultConfig(projectRoot);
+config.watchFolders = [workspaceRoot];
+
+module.exports = config;
